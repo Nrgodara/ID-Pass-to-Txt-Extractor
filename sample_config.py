6f1703379335a70192ad713083f6e4eedfa72a11
Nrgodara <155721977+Nrgodara@users.noreply.github.com>
2024-05-10 12:28:57 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,9 +1,9 @@
 import os
 
-api_id = 3748059
-api_hash = os.environ.get("API_HASH", "f8c9df448f3ba20a900bc2ffc8dae9d5")
-bot_token = os.environ.get("BOT_TOKEN")
-auth_users = os.environ.get("AUTH_USERS", "5591734243,1369808729")
+api_id = 29251040
+api_hash = os.environ.get("API_HASH", "a1de34e4ad21cc9fb34798473eaf7bab")
+bot_token = os.environ.get("BOT_TOKEN", "7062176093:AAHM8aqhPNbxiVa_JMZ0msApxuQOX1IpG24")
+auth_users = os.environ.get("AUTH_USERS", "6539854720,1280494242")
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = os.environ.get("OWNER_USERS", "5591734243,1369808729")
+osowner_users = os.environ.get("OWNER_USERS", "1280494242")
 owner_users = [int(num) for num in osowner_users.split(",")]
